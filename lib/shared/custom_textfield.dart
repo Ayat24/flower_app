94b26e74a4eba3dba28297a431e906cc744dbffc
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-23 17:25:27 +0200

    create custom text field

--- lib/shared/custom_textfield.dart
+++ lib/shared/custom_textfield.dart
@@ -1,1 +1,26 @@
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+
+class Custom_text_Field extends StatelessWidget {
+  const Custom_text_Field({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.text,
+      obscureText: false,
+      decoration: InputDecoration(
+        hintText: "Enter your Email;",
+        enabledBorder: OutlineInputBorder(
+          borderSide: Divider.createBorderSide(context),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        filled: true,
+        contentPadding: const EdgeInsets.all(10),
+      ),
+    );
+  }
+}
