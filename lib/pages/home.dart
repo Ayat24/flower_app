d306db14c87d2f611a2014de281a9228c7dd0e81
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-27 17:04:14 +0200

    add position to move stack

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,15 +14,18 @@ class Home extends StatelessWidget {
             children: [
               Stack(
                 children: [
-                  Container(
-                      child: Text(
-                        "8",
-                        style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                      ),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(211, 164, 255, 193),
-                          shape: BoxShape.circle)),
+                  Positioned(
+                    bottom: 10,
+                    child: Container(
+                        child: Text(
+                          "8",
+                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(211, 164, 255, 193),
+                            shape: BoxShape.circle)),
+                  ),
                   IconButton(
                       onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
                 ],
