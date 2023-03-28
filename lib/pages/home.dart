0c5aab8bc78afd637cf0dd0fd4c9491e815a99a9
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-28 18:16:01 +0200

    add itemCount and itemBuilder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,9 +12,14 @@ class Home extends StatelessWidget {
 crossAxisCount: 2,
 childAspectRatio: 3 / 2,
 crossAxisSpacing: 10,
-mainAxisSpacing: 33),
+mainAxisSpacing: 33,),
+ itemCount: 4,
+ itemBuilder: (BuildContext context, int index) {
+    
+ }
+),
 
-         )
+    
          
          ),,
       drawer: Drawer(
