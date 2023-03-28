db3145ac8fef569976a5f931da17cbc0270ced9b
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-28 20:29:55 +0200

    add appBar to details screen

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_appl/shared/colors.dart';
 
 class Details extends StatelessWidget {
   const Details({super.key});
@@ -6,6 +7,39 @@ class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appbarGreen,
+        title: Text("Details screen"),
+        leading: Text("data"),
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    bottom: 10,
+                    child: Container(
+                        child: Text(
+                          "8",
+                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(211, 164, 255, 193),
+                            shape: BoxShape.circle)),
+                  ),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text("\$ 128"),
+              )
+            ],
+          )
+        ],
+      ),
       body: Text("details"),
     );
   }
