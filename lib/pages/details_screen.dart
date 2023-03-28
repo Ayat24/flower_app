7b27fe49cdba3eaa794c4ce6dbcd5a6dd231ce0d
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-28 21:31:18 +0200

    finish this part

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -14,21 +14,19 @@ class _DetailsState extends State<Details> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: appbarGreen,
-          title: Text("Details screen"),
-          leading: Text("data"),
           actions: [
             Row(
               children: [
                 Stack(
                   children: [
                     Positioned(
-                      bottom: 10,
+                      bottom: 24,
                       child: Container(
                           child: Text(
                             "8",
-                            style:
-                                TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 0, 0, 0)),
                           ),
                           padding: EdgeInsets.all(5),
                           decoration: BoxDecoration(
@@ -36,132 +34,141 @@ class _DetailsState extends State<Details> {
                               shape: BoxShape.circle)),
                     ),
                     IconButton(
-                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                      onPressed: () {},
+                      icon: Icon(Icons.add_shopping_cart),
+                    ),
                   ],
                 ),
                 Padding(
                   padding: const EdgeInsets.only(right: 12),
-                  child: Text("\$ 128"),
-                )
+                  child: Text(
+                    "\$ 13",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
               ],
-            )
+            ),
           ],
+          backgroundColor: appbarGreen,
+          title: Text("Details screen"),
         ),
-        body: Column(
-          children: [
-            Image.asset("assets/img/2.webp"),
-            SizedBox(
-              height: 11,
-            ),
-            Text(
-              "\$ 12.99",
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                    padding: EdgeInsets.all(4),
-                    child: Text(
-                      "New",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 129, 129),
-                      borderRadius: BorderRadius.circular(4),
-                    )),
-                SizedBox(
-                  width: 8,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: 66,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.edit_location,
-                      size: 26,
-                      color: Color.fromARGB(168, 3, 65, 27),
-                      // color: Color.fromARGB(255, 186, 30, 30),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Text(
-                      "Flower Shop",
-                      style: TextStyle(fontSize: 19),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/img/2.webp"),
+              SizedBox(
+                height: 11,
+              ),
+              Text(
+                "\$ 12.99",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                      padding: EdgeInsets.all(4),
                       child: Text(
-                        "Details : ",
-                        style: TextStyle(fontSize: 22),
-                        textAlign: TextAlign.start,
+                        "New",
+                        style: TextStyle(fontSize: 15),
                       ),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      "A flower, sometimes known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Angiospermae). The biological function of a flower is to facilitate reproduction, usually by providing a mechanism for the union of sperm with eggs. Flowers may facilitate outcrossing (fusion of sperm and eggs from different individuals in a population) resulting from cross-pollination or allow selfing (fusion of sperm and egg from the same flower) when self-pollination occurs.",
-                      style: TextStyle(
-                        fontSize: 18,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 255, 129, 129),
+                        borderRadius: BorderRadius.circular(4),
+                      )),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
                       ),
-                      maxLines: isShowMore ? 3 : null,
-                      overflow: TextOverflow.fade,
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 66,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.edit_location,
+                        size: 26,
+                        color: Color.fromARGB(168, 3, 65, 27),
+                        // color: Color.fromARGB(255, 186, 30, 30),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        "Flower Shop",
+                        style: TextStyle(fontSize: 19),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Text(
+                  "Details : ",
+                  style: TextStyle(fontSize: 22),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "A flower, sometimes known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Angiospermae). The biological function of a flower is to facilitate reproduction, usually by providing a mechanism for the union of sperm with eggs. Flowers may facilitate outcrossing (fusion of sperm and eggs from different individuals in a population) resulting from cross-pollination or allow selfing (fusion of sperm and egg from the same flower) when self-pollination occurs.",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                maxLines: isShowMore ? 3 : null,
+                overflow: TextOverflow.fade,
+              ),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isShowMore = !isShowMore;
+                    });
+                  },
+                  child: Text(
+                    isShowMore ? "Show more" : "Show less",
+                    style: TextStyle(
+                      fontSize: 18,
                     ),
-                    TextButton(
-                        onPressed: () {
-                          setState(() {
-                            isShowMore = !isShowMore;
-                          });
-                        },
-                        child: Text(
-                          isShowMore ? "Show more" : "Show less",
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
-                        ))
-                  ],
-                )
-              ],
-            ),
-          ],
+                  ))
+            ],
+          ),
         ));
   }
 }
