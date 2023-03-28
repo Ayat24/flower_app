131234ae3b2b93e72fda3ddea340ae035e14196e
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-28 20:48:34 +0200

    add row

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -7,54 +7,55 @@ class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: appbarGreen,
-        title: Text("Details screen"),
-        leading: Text("data"),
-        actions: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                    bottom: 10,
-                    child: Container(
-                        child: Text(
-                          "8",
-                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(211, 164, 255, 193),
-                            shape: BoxShape.circle)),
-                  ),
-                  IconButton(
-                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 12),
-                child: Text("\$ 128"),
-              )
-            ],
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Image.asset("assets/img/2.webp"),
-          SizedBox(
-            height: 11,
-          ),
-          Text(
-            "\$ 12.99",
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: appbarGreen,
+          title: Text("Details screen"),
+          leading: Text("data"),
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                      bottom: 10,
+                      child: Container(
+                          child: Text(
+                            "8",
+                            style:
+                                TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle)),
+                    ),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text("\$ 128"),
+                )
+              ],
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Image.asset("assets/img/2.webp"),
+            SizedBox(
+              height: 11,
+            ),
+            Text(
+              "\$ 12.99",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row()
+          ],
+        ));
   }
 }
