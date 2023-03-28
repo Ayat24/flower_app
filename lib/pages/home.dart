0ac1796219f287e6f00b6165f4e47af2ac8406cb
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-28 18:05:05 +0200

    create  body

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,6 +7,9 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body:
+      
+      
       drawer: Drawer(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
