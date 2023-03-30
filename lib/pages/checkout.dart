546b29fcf4bbffa23363bf9cbce8a7cdf7aa655f
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-30 12:47:36 +0200

    add ListTile

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_appl/model/item.dart';
 import 'package:flutter_appl/shared/colors.dart';
 import 'package:flutter_appl/shared/appbar.dart';
+
 class CheckOut extends StatelessWidget {
   const CheckOut({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: appbarGreen,
         title: Text("checkout screen"),
         actions: [ProductsAndPrice()],
@@ -23,15 +24,26 @@ class CheckOut extends StatelessWidget {
                   itemCount: Carttt.selectedProducts.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Card(
-                      child: 
+                      child: ListTile(
+                        title: Text(Carttt.selectedProducts[index].name),
+                        subtitle: Text(
+                            "${Carttt.selectedProducts[index].price} - ${Carttt.selectedProducts[index].location}"),
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage(
+                              Carttt.selectedProducts[index].imgPath),
+                        ),
+                        trailing: IconButton(
+                            onPressed: () {
+                              Carttt.delete(Carttt.selectedProducts[index]);
+                            },
+                            icon: Icon(Icons.remove)),
+                      ),
                     );
                   }),
             ),
           ),
-     
         ],
       ),
     );
   }
 }
-   
