718dca64b63e566386b4ceb953a7f304d101f6a2
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-30 11:46:12 +0200

    add price

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -3,7 +3,9 @@ import 'package:flutter_appl/model/item.dart';
 
 class Cart with ChangeNotifier {
   List selectedProducts = [];
+  int price = 0;
   add(Item product) {
     selectedProducts.add(product);
+    price += product.price.round();
   }
 }
