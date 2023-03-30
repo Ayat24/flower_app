c5d1bd8c32446ab5e35ac51adf7364e422a06960
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-30 11:16:38 +0200

    install provider

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+    return widget(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
     );
   }
 }
