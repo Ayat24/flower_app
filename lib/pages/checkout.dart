34455b1c30437b966df64bc99d9b9314a24c9b86
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-30 12:46:53 +0200

    add SingleChildScrollView

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -2,21 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_appl/model/item.dart';
 import 'package:flutter_appl/shared/colors.dart';
 import 'package:flutter_appl/shared/appbar.dart';
-
 class CheckOut extends StatelessWidget {
   const CheckOut({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
         backgroundColor: appbarGreen,
         title: Text("checkout screen"),
         actions: [ProductsAndPrice()],
       ),
       body: Column(
-        children: [],
+        children: [
+          SingleChildScrollView(
+            child: SizedBox(
+              height: 550,
+              child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: Carttt.selectedProducts.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: 
+                    );
+                  }),
+            ),
+          ),
+     
+        ],
       ),
     );
   }
 }
+   
