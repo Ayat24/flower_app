1934c2113f700436ebd042f571c51fefe14ea864
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-30 12:41:00 +0200

    add backgroundColor

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -9,7 +9,9 @@ class CheckOut extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: appbarGreen,
+      ),
     );
   }
 }
