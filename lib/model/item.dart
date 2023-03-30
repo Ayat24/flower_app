5634dd0a76d183888b02802a313af1840321e031
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-30 11:02:19 +0200

    add location to item

--- lib/model/item.dart
+++ lib/model/item.dart
@@ -1,11 +1,16 @@
 class Item {
   String imgPath;
   double price;
-  Item({required this.imgPath, required this.price});
+  String location;
+
+  Item(
+      {required this.imgPath,
+      required this.price,
+      this.location = "Main branch"});
 }
 
 final List<Item> items = [
-  Item(price: 12.99, imgPath: "assets/img/1.webp"),
+  Item(price: 12.99, imgPath: "assets/img/1.webp", location: "Ali shop"),
   Item(price: 12.99, imgPath: "assets/img/2.webp"),
   Item(price: 12.99, imgPath: "assets/img/3.webp"),
   Item(price: 12.99, imgPath: "assets/img/4.webp"),
